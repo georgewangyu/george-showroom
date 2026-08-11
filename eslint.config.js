f1fe90518819e31063998f59270155acf5a81c69
georgewangyu <gywang912@gmail.com>
2026-08-10 20:47:03 -0700

    chore: move private artifacts out of public repo

--- eslint.config.js
+++ eslint.config.js
@@ -4,28 +4,17 @@ import eslintConfigPrettier from "eslint-config-prettier/flat";
 import { defineConfig } from "eslint/config";
 import globals from "globals";
 
-export default defineConfig(
-  js.configs.recommended,
-  eslintConfigPrettier,
-  {
-    files: ["**/*.js"],
-    ignores: ["dist/", "node_modules/", ".lavish-axi/", "lavish-code-review.html"],
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-      globals: {
-        ...globals.node,
-      },
-    },
-    rules: {
-      "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+export default defineConfig(js.configs.recommended, eslintConfigPrettier, {
+  files: ["**/*.js"],
+  ignores: ["dist/", "node_modules/", ".lavish-axi/", "lavish-code-review.html"],
+  languageOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    globals: {
+      ...globals.node,
     },
   },
-  {
-    files: ["examples/george-finance/*.js"],
-    languageOptions: {
-      sourceType: "script",
-      globals: globals.browser,
-    },
+  rules: {
+    "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
   },
-);
+});
